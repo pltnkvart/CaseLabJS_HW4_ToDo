c0d78b9bf85218d619b5bf6f5c6298479e10bcdb
pltnkvart <artosi@yandex.ru>
2023-10-15 14:49:07 +0300

    Modified adding users

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const UsersUrl = 'https://jsonplaceholder.typicode.com/users/'
-const ToDoURL = 'https://jsonplaceholder.typicode.com/todos'
+const ToDoURL = 'https://jsonplaceholder.typicode.com/todos/'
+
+let usersData = [];
+let todoData = [];
 
 function getUsers() {
     const selectUser = document.getElementById('user-todo')
     fetch(UsersUrl)
         .then(response => response.json())
-        .then(users => {
+        .then(users => { 
             users.forEach(user => {
+                usersData.push(user);
                 const userSelect = document.createElement('option');
-                userSelect.innerHTML = `${user.name}`
+                userSelect.innerHTML = `${user.name}`;
                 selectUser.appendChild(userSelect);
             });
+            getToDos();
         })
         .catch(error => {
             alert(error);
         })
 }
 
-function searchUser(todo, users) {
-    const user = users.find(user => user.id === todo.userId);
-    return user.name;
+function searchUser(todo) {
+    const userFind = usersData.find(user => user.id === todo.userId);
+    return userFind.name;
 }
 
 function getToDos() {
@@ -27,27 +32,19 @@ function getToDos() {
     fetch(ToDoURL)
         .then(response => response.json())
         .then(todos => {
-            fetch(UsersUrl)
-                .then(response => response.json())
-                .then(users => {
-                    todos.forEach(todo => {
-                        const elem = document.createElement('li');
-                        elem.classList.add("todo-item");
-                        elem.innerHTML = `<input type="checkbox" id="isCompleted" ${todo.completed ? "checked" : ""}>
-                                <span>${todo.title} <em>by</em> <b>${searchUser(todo, users)}</b></span>`
-                        listToDos.appendChild(elem);
-                    });
-                })
-                .catch(error => {
-                    alert(error);
-                })
+            todoData = todos;
+            todos.forEach(todo => {
+                const elem = document.createElement('li');
+                elem.classList.add("todo-item");
+                elem.innerHTML = `<input type="checkbox" id="isCompleted" ${todo.completed ? "checked" : ""}>
+                                <span>${todo.title} <em>by</em> <b>${searchUser(todo)}</b></span>`
+                listToDos.appendChild(elem);
+            });
         })
         .catch(error => {
             alert(error);
         })
 }
 
-getUsers()
-getToDos()
 
-
+getUsers();
